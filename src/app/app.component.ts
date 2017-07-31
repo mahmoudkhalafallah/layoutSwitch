772229ce110a539fb2a6ebc3481add617bbe7479
Mahmoud <mahmoudkhalafallah92@gmail.com>
2017-07-31 22:05:20 +0200

    initial commit, loading data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticlesService } from './services/articles.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ArticlesService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor(private articleSer: ArticlesService) { }
+
+  ngOnInit() {
+    // loading data
+    this.articleSer.getArticles().subscribe(res => {
+      console.log(res);
+    })
+  }
 }
